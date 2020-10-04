cace01844484ef8abbe243e6cb84ea69748ecdfe
Pip Elliott <pj.elliott232323@gmail.com>
2020-10-04 17:16:58 +0200

    e3.rb

---
+++ chapter_1/e3.rb
@@ -0,0 +1,7 @@
+movies = { :memento => 1999,
+            :interstellar => 2018,
+              :loopers => 2005}
+
+            puts movies[:memento]
+            puts movies[:interstellar]
+            puts movies[:loopers]
