229748ee5a625907bfffeffe49ce1c4ea76bb9a4
Pip Elliott <pj.elliott232323@gmail.com>
2020-10-09 14:13:24 +0200

    chapter complete

--- chapter_3/mutate.rb
+++ chapter_3/mutate.rb
@@ -1,10 +1,10 @@
 a = [1, 2, 3]
 
-# Example of a method definition that modifies its argument permanently
-def mutate(array)
-  array.pop
+# Example of a method definition that does not mutate the caller
+def no_mutate(array)
+  array.last
 end
 
- puts "Before mutate method: #{a}"
-mutate(a)
-puts "After mutate method: #{a}"
+p "Before no_mutate method: #{a}"
+no_mutate(a)
+p "After no_mutate method: #{a}"
