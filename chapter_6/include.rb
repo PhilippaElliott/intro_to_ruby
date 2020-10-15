978c08f5eff0665162efb1956351a3d95d7c27c6
Pip Elliott <pj.elliott232323@gmail.com>
2020-10-15 16:25:19 +0200

    with brackets around number but nor sure why it is printing 6 times.

--- chapter_6/include.rb
+++ chapter_6/include.rb
@@ -2,7 +2,7 @@ arr = [1, 3, 5, 7, 9, 11]
 number = 3
 
 arr.each do |num|
-  if arr.include? number
+  if arr.include? (number)
     puts "#{number} is in the array"
   else puts "#{number} is not in the array"
   end
