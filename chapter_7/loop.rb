25cbe0a1f630286238bedfb50a623ab7470d7220
Pip Elliott <pj.elliott232323@gmail.com>
2020-10-17 12:12:04 +0200

    need to change so final output is as a sentence

---
+++ chapter_7/loop.rb
@@ -0,0 +1,6 @@
+family = { mother: "Judy", Father: "David", Grandmother: "Renee", Daughter: "Nora", Son: "Thomas"}
+
+family.each {|key, value| puts key}
+family.each {|key, value| puts value}
+
+puts family
